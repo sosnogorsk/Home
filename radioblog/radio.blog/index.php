c7840c552afe6b90990b5e15ce3d37ebbddfc2da
Vi Ha <71669364+sosnogorsk@users.noreply.github.com>
2025-03-09 13:21:16 -0700

    Add files via upload

---
+++ radioblog/radio.blog/index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>RADIO.BLOG</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0"
+marginheight="0"> <object
+classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
+codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#3,0,0,0"
+width="220" height="300"> <param name="src" value="=radio.swf?autoplay=<?
+echo $autoplay; ?>"> <param name="quality" value="high"> <param
+name="wmode" value="transparent"> <embed src="=radio.swf?autoplay=<? echo
+$autoplay; ?>" TYPE="application/x-shockwave-flash"
+PLUGINSPAGE="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash"
+width="220" height="300"> </object><br> 
+</body>
+</html>
